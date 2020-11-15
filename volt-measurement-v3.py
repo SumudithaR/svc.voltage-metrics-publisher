06b1a0281c58e2b9986daa060591ab753c57c2a1
Sumuditha Ranawaka <sumuditha.ranawaka@hotmail.com>
2020-11-15 15:54:54 +0000

    Testing V3 Script

--- volt-measurement-v3.py
+++ volt-measurement-v3.py
@@ -13,15 +13,15 @@ import time
 
 
 class RawMetricDto():
-    voltage0 = 0
-    voltage1 = 0
-    voltage2 = 0
-    voltage3 = 0
-    voltage4 = 0
-    voltage5 = 0
-    voltage6 = 0
-    voltage7 = 0
-    deviceTime = None
+    voltage0 = 0.0
+    voltage1 = 0.0
+    voltage2 = 0.0
+    voltage3 = 0.0
+    voltage4 = 0.0
+    voltage5 = 0.0
+    voltage6 = 0.0
+    voltage7 = 0.0
+    deviceTime = ""
 
 
 debug = False
