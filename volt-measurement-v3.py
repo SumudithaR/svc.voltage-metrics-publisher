bb704c169c81c77dcb5a3071ae415771ba6159d5
Sumuditha Ranawaka <sumuditha.ranawaka@hotmail.com>
2020-11-17 14:01:14 +0000

    Testing V3 Script 2

--- volt-measurement-v3.py
+++ volt-measurement-v3.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 from kafka import KafkaProducer
-#import socket
 import json
-#import time
-# import datetime
 from gpiozero import MCP3008  # Installed in GAM 13/09/2019.
 from time import sleep       #
-#import threading
-#import sched
 import time
+import requests
+import threading
 #
 
 
@@ -24,39 +21,14 @@ class RawMetricDto():
     deviceTime = ""
 
 
-debug = False
+debug = True
 vref = 3.31
-topicName = "raw-voltage-metrics"
-kafkaProducer = None
-#fileLock = threading.Lock()
-#mcpLock = threading.Lock()
-schedulerInterval = 30
-
-try:
-    if(debug):
-        print("[ControlSystemOne] Starting Kafka Service.")
-
-    kafkaProducer = KafkaProducer(
-        bootstrap_servers='walpola.tk:9094', batch_size=0)
-
-except Exception as ex:
-    if(debug):
-        print("[ControlSystemOne] Failed to connect to Kafka Host.")
-        print(ex)
 
 
-# def on_send_success(record_metadata):
-#     if(debug):
-#         print(record_metadata.topic)
-#         print(record_metadata.partition)
-#         print(record_metadata.offset)
+def publishToApi(jsonModel):
+    requests.post("https://walpola.tk/metrics-publish", jsonModel)
 
 
-# def on_send_error(excp):
-#     if(debug):
-#         #log.error('I am an errback', exc_info=excp)
-#         # # handle exception
-#         print(excp)
 def getDigitalValue(channel):
     try:
         adc = MCP3008(channel=channel)
@@ -69,59 +41,57 @@ def getDigitalValue(channel):
 
 while True:
 
-    adc0 = getDigitalValue(0)
-    adc1 = getDigitalValue(1)
-    adc2 = getDigitalValue(2)
-    adc3 = getDigitalValue(3)
-    adc4 = getDigitalValue(4)
-    adc5 = getDigitalValue(5)
-    adc6 = getDigitalValue(6)
-    adc7 = getDigitalValue(7)
+    # adc0 = getDigitalValue(0)
+    # adc1 = getDigitalValue(1)
+    # adc2 = getDigitalValue(2)
+    # adc3 = getDigitalValue(3)
+    # adc4 = getDigitalValue(4)
+    # adc5 = getDigitalValue(5)
+    # adc6 = getDigitalValue(6)
+    # adc7 = getDigitalValue(7)
 
-    voltage0 = vref*4.57*adc0  # Battery
-    voltage1 = vref*4.57*adc1  # Bus
-    voltage2 = vref*4.57*adc2  # Router
-    voltage3 = vref*4.57*adc3  # Pi7 Voltage
-    voltage4 = vref*adc4  # XX3
-    voltage5 = vref*adc5  # XX4
-    voltage6 = vref*adc6  # WTL
-    voltage7 = vref*adc7  # WLL
+    # voltage0 = vref*4.57*adc0  # Battery
+    # voltage1 = vref*4.57*adc1  # Bus
+    # voltage2 = vref*4.57*adc2  # Router
+    # voltage3 = vref*4.57*adc3  # Pi7 Voltage
+    # voltage4 = vref*adc4  # XX3
+    # voltage5 = vref*adc5  # XX4
+    # voltage6 = vref*adc6  # WTL
+    # voltage7 = vref*adc7  # WLL
 
     localtime = time.asctime(time.localtime(time.time()))
 
-    print("Bat1:", '{:.1f}'.format(voltage0), "V," "Bus:", '{:.1f}'.format(voltage1), "V," "Rou:",
-          '{:.1f}'.format(voltage2), "V," "Bat2:", '{:.1f}'.format(voltage3), "V,", localtime)
-    fo = open("/Camgam/udin/GampahaLog.txt", "a")
-    L1 = ['{:.1f}'.format(voltage0), ",", '{:.1f}'.format(voltage1), ",", '{:.1f}'.format(voltage2), ",", '{:.1f}'.format(voltage3), ",", '{:.1f}'.format(
-        voltage4), ",", '{:.1f}'.format(voltage5), ",", '{:.1f}'.format(voltage6), ",", '{:.1f}'.format(voltage7), ",", localtime]
-    fo.writelines(L1)
-    fo.write('\n')
-    fo.close()
+    # print("Bat1:", '{:.1f}'.format(voltage0), "V," "Bus:", '{:.1f}'.format(voltage1), "V," "Rou:",
+    #       '{:.1f}'.format(voltage2), "V," "Bat2:", '{:.1f}'.format(voltage3), "V,", localtime)
+    # fo = open("/Camgam/udin/GampahaLog.txt", "a")
+    # L1 = ['{:.1f}'.format(voltage0), ",", '{:.1f}'.format(voltage1), ",", '{:.1f}'.format(voltage2), ",", '{:.1f}'.format(voltage3), ",", '{:.1f}'.format(
+    #     voltage4), ",", '{:.1f}'.format(voltage5), ",", '{:.1f}'.format(voltage6), ",", '{:.1f}'.format(voltage7), ",", localtime]
+    # fo.writelines(L1)
+    # fo.write('\n')
+    # fo.close()
 
     try:
-        if kafkaProducer is not None:
-            if(debug):
-                print("[ControlSystemOne] Starting Voltage Metrics Publish.")
+        if(debug):
+            print("[ControlSystemOne] Starting Voltage Metrics Publish.")
 
-            model = RawMetricDto()
+        model = RawMetricDto()
 
-            model.voltage0 = voltage0  # Battery-Main
-            model.voltage1 = voltage1  # Bus
-            model.voltage2 = voltage2  # Router
-            model.voltage3 = voltage3  # Battery-Emg.Lamps
-            model.voltage4 = voltage4  # XX3
-            model.voltage5 = voltage5  # XX4
-            model.voltage6 = voltage6  # WTL
-            model.voltage7 = voltage7  # WLL
-            model.deviceTime = localtime
+        # model.voltage0 = voltage0  # Battery-Main
+        # model.voltage1 = voltage1  # Bus
+        # model.voltage2 = voltage2  # Router
+        # model.voltage3 = voltage3  # Battery-Emg.Lamps
+        # model.voltage4 = voltage4  # XX3
+        # model.voltage5 = voltage5  # XX4
+        # model.voltage6 = voltage6  # WTL
+        # model.voltage7 = voltage7  # WLL
+        model.deviceTime = localtime
+        
+        jsonModel = json.dumps(model.__dict__)
 
-            jsonModel = json.dumps(model.__dict__)
-            jsonBytes = bytes(jsonModel, 'utf-8')
-            # .add_callback(on_send_success).add_errback(on_send_error)
-            kafkaProducer.send(topic=topicName, value=jsonBytes)
+        threading.Thread(target=publishToApi, args=(jsonModel,), daemon=True).run()
 
-            if(debug):
-                print("[ControlSystemOne] Metrics Publish Complete.")
+        if(debug):
+            print("[ControlSystemOne] Metrics Publish Complete.")
 
     except Exception as ex:
         if(debug):
@@ -129,15 +99,3 @@ while True:
             print(ex)
 
     sleep(30)
-
-
-# def getVoltages():
-#     extractionScheduler.enter(schedulerInterval, 0, getVoltages)
-
-    # if(adc0 is None or adc1 is None or adc2 is None or adc3 is None or adc4 is None or adc5 is None or adc6 is None or adc7 is None):
-    #     return
-
-
-# extractionScheduler = sched.scheduler(time.time, time.sleep)
-# extractionScheduler.enter(schedulerInterval, 0, getVoltages)
-# extractionScheduler.run()
